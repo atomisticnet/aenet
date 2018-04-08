8aade28842d9b48d6ee7daf2abbfef94e02819ec
Nongnuch Artrith <nartrith@atomistic.net>
2018-04-08 21:32:38 +0100

    [license] License changed to MPL v2

--- src/ext/arglib.f90
+++ src/ext/arglib.f90
@@ -3,20 +3,16 @@
 !-----------------------------------------------------------------------
 !+ This file is part of the AENET package.
 !+
-!+ Copyright (C) 2012-2017 Nongnuch Artrith and Alexander Urban
+!+ Copyright (C) 2012-2018 Nongnuch Artrith and Alexander Urban
 !+
-!+ This program is free software: you can redistribute it and/or modify
-!+ it under the terms of the GNU General Public License as published by
-!+ the Free Software Foundation, either version 3 of the License, or
-!+ (at your option) any later version.
+!+ This Source Code Form is subject to the terms of the Mozilla Public
+!+ License, v. 2.0. If a copy of the MPL was not distributed with this
+!+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 !+
 !+ This program is distributed in the hope that it will be useful, but
 !+ WITHOUT ANY WARRANTY; without even the implied warranty of
-!+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-!+ General Public License for more details.
-!+
-!+ You should have received a copy of the GNU General Public License
-!+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!+ Mozilla Public License, v. 2.0, for more details.
 !-----------------------------------------------------------------------
 ! 2011-02-18 Alexander Urban (AU), Nongnuch Artrith (NA)
 ! 2011-03-19 -- new interface: args_env()
