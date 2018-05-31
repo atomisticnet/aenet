85a248189ad16578f3a3cabdf1e84017f4b8fc04
John Kitchin <kitchin@google.com>
2018-05-31 16:51:32 -0700

    Update the tests for version 2.
    One duplicate test was deleted. The v1 potentials were deleted, and replaced
    with v2 potentials.

--- python3/aenet/tests/test_core.py
+++ python3/aenet/tests/test_core.py
@@ -1,12 +1,11 @@
-"""
-Tests the aenet interface in core.py.
+"""Tests the aenet interface in core.py.
 """
 
 from __future__ import print_function, division
 
 __author__ = "Nonguch Artrith, Alexander Urban"
 __email__ = "nartrith@mit.edu, alexurba@mit.edu"
-__date__   = "2014-09-11"
+__date__ = "2014-09-11"
 __version__ = "0.1"
 
 import unittest
@@ -14,28 +13,23 @@ import os
 
 from aenet.core import ANNPotentials
 
-POTENTIAL_Ti = os.path.join(os.path.dirname(__file__), 'Ti.10t-10t.nn')
-POTENTIAL_O  = os.path.join(os.path.dirname(__file__), 'O.10t-10t.nn')
-POTENTIAL_H  = os.path.join(os.path.dirname(__file__), 'H.10t-10t.nn')
+POTENTIAL_Ti = os.path.join(os.path.dirname(__file__), "Ti.15t-15t.nn")
+POTENTIAL_O = os.path.join(os.path.dirname(__file__), "O.15t-15t.nn")
+
 potential_files = {
-    'Ti' : POTENTIAL_Ti,
-    'O'  : POTENTIAL_O,
-    'H'  : POTENTIAL_H,
+    "Ti": POTENTIAL_Ti,
+    "O": POTENTIAL_O,
 }
+
 
 class AenetCoreTest(unittest.TestCase):
 
-    def test_load_potentials(self):
-        pot = ANNPotentials(potential_files)
-        self.assertEqual(pot.Rc_min, 0.75)
-        self.assertEqual(pot.Rc_max, 6.5)
-        del pot
+  def test_load_potentials(self):
+    pot = ANNPotentials(potential_files)
+    self.assertEqual(pot.Rc_min, 0.55)
+    self.assertEqual(pot.Rc_max, 8.0)
+    del pot
 
-    def test_atomic_energy(self):
-        pot = ANNPotentials(potential_files)
-        self.assertEqual(pot.Rc_min, 0.75)
-        self.assertEqual(pot.Rc_max, 6.5)
-        del pot
 
-if __name__=="__main__":
-    unittest.main()
+if __name__ == "__main__":
+  unittest.main()
