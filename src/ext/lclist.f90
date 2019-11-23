55cd9113b66b6d3d79e457351986d123e41403bc
Nongnuch Artrith <nartrith@atomistic.net>
2019-11-23 12:39:18 -0500

    License header updated with references

--- src/ext/lclist.f90
+++ src/ext/lclist.f90
@@ -13,6 +13,13 @@
 !+ WITHOUT ANY WARRANTY; without even the implied warranty of
 !+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 !+ Mozilla Public License, v. 2.0, for more details.
+!+ ---------------------------------------------------------------------
+!+ If you make use of AENET for your publication, please cite:
+!+ [1] N. Artrith and A. Urban, Comput. Mater. Sci. 114 (2016) 135-150.
+!+ [2] J. Behler and M. Parrinello, Phys. Rev. Lett. 98 (2007) 146401.
+!+
+!+ If you used the Chebyshev descriptor, please cite:
+!+ [3] N. Artrith, A. Urban, and G. Ceder, PRB 96 (2017) 014112.
 !----------------------------------------------------------------------
 
 module lclist
